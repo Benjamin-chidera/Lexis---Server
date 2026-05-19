226fba752cf5ebaf4b30f58d9fd5bbc5e53d4d10
Discover Benix <mac@MACs-MacBook-Pro-3.local>
2026-05-19 11:28:11 +0100

    feat: add production url to cors and web socket

--- main.py
+++ main.py
@@ -138,6 +138,8 @@ fastapi_app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://127.0.0.1:3000",
+        "https://lexis-pi.vercel.app/"
+        "https://lexis-pi.vercel.app/login"
     ],
     allow_credentials=True,
     allow_methods=["*"],
