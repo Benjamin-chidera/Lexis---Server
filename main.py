34f78ea50603b801a7d181accbb7f3143512963c
Discover Benix <mac@MACs-MacBook-Pro-3.local>
2026-06-20 12:24:45 +0100

    feat: add production url to cors origin

--- main.py
+++ main.py
@@ -146,6 +146,7 @@ fastapi_app.add_middleware(
         "http://127.0.0.1:5174",
         "http://127.0.0.1:3000",
         "https://lexis-pi.vercel.app",
+        "https://lexis.discoverbenix.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
